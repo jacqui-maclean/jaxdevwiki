192f4fb22878e5b54cb1ee30c571c1707546c1db
jacqui-maclean <inaripaddler@gmail.com>
2023-07-25 12:49:14 +0100

    add FileList component

---
+++ src/components/FileList.tsx
@@ -0,0 +1,71 @@
+import { Product } from "../App";
+import { useState } from "react";
+import SearchInput from "./SearchInput";
+import { CgNotes } from "react-icons/cg";
+import Category from "./Category";
+import CardGrid from "./CardGrid";
+import Card from "./CardSimple";
+
+interface Props {
+  products: Product[];
+}
+
+const FileList = ({ products }: Props) => {
+  const [foundItems, setFoundItems] = useState<Product[]>();
+  const handleSearch = (searchTerm: string) => {
+    let searchResult = products?.filter((item) =>
+      item.tags.includes(searchTerm)
+    );
+    if (searchResult.length > 0) {
+      setFoundItems(searchResult);
+    }
+  };
+  //separate the data into two arrays found/not found so we can display them separately
+  let inputData: Product[] | undefined = foundItems
+    ? products?.filter(
+        (product) =>
+          product.title !==
+          foundItems.find((item) => item.title === product.title)?.title
+        //find the item in the foundItems array that matches the product title
+        //if it matches then filter it out of the inputData array
+      )
+    : products;
+
+  return (
+    <>
+      <SearchInput onSearch={handleSearch} />
+      <div className="d-flex flex-wrap justify-content-around align-items-center">
+        {foundItems
+          ? foundItems.map((product: Product) => {
+              return (
+                <div
+                  key={product.slug}
+                  style={{
+                    color: "dodgerblue",
+                  }}
+                >
+                  {product.title}
+                </div>
+              );
+            })
+          : null}
+      </div>
+      <CardGrid>
+        <Card>
+          <Category products={inputData} heading="array" />
+        </Card>
+        <Card>
+          <Category products={inputData} heading="functions" />
+        </Card>
+        <Card>
+          <Category products={inputData} heading="objects" />
+        </Card>
+        <Card>
+          <Category products={inputData} heading="variables" />
+        </Card>
+      </CardGrid>
+    </>
+  );
+};
+
+export default FileList;
